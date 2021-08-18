2e28b0557707f33147bae70db73c2e6b895e031e
Michael Pace <michael.g.pace@gmail.com>
2021-08-18 12:14:11 -0500

    specified image clicked on local storage, other stuff

--- js/results.js
+++ js/results.js
@@ -3,18 +3,9 @@ console.log('js file is connected');
 
 // global variables
 
-let imageElements = document.getElementsByTagName('img');
-console.log('image elements', imageElements);
-
-// movie object constructor function
-
-function Movie(name, imagesrc, rating)
-
-// Create movie objects
-
-new Movie();
-
-// code to get an extra random 3?
-// let nextMovieIndex1 = Math.floor(Math.random() * allpgComedies.length);
-// let nextMovieIndex2 = Math.floor(Math.random() * allProducts.length);
-// let nextMovieIndex3 = Math.floor(Math.random() * allProducts.length);
+function randomizer(){
+    picks = localStorage.getItem("picks");
+    if (picks == comedy && picks == pg){
+        let pickedImages = document.getElementById('pgcomedies')
+    }
+}
