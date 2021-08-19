6dfbc2d2998fcbb1c8a7d0615375a0ad11debfc7
Michael Pace <michael.g.pace@gmail.com>
2021-08-19 11:59:24 -0500

    fixed create profile button, added newpage

--- js/results.js
+++ js/results.js
@@ -20,15 +20,15 @@ if (picks === '1' && rpicks === 'PG'){
     let randomPGComedyMovie = pgcomedy[Math.floor(Math.random() * pgcomedy.length)];
 
 }
-document.getElementById("mybutton").onclick = function(){
-    location.href = "www.google.com";
-};
-let bclick = document.getElementById('needID');
+// document.getElementById("mybutton").onclick = function(){
+//     location.href = "www.google.com";
+// };
+let bclick = document.getElementById('mybutton');
 
 bclick.onclick = function(){
-    alert('hshj')
+    alert('hshj');
     // window.location.href = this.id + 'index.html';
-}
+};
 console.log('rando', randomPGComedyMovie);
 
 
